adb2a471edf5b84e76f8afcc26f8fd05f10997c5
Jayantagit <jayantamandal2006@gmail.com>
2022-12-05 17:58:33 +0530

    First Commit

---
+++ test/specs/validation.js
@@ -0,0 +1,24 @@
+describe('Verify error messages', () => {
+    it('capture error messages and validate', async () => {
+
+        await browser.url("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+
+        await browser.maximizeWindow()
+
+        const title = await browser.getTitle()
+        console.log("Page Title :"+title)
+
+        const user = await $("//input[@name='username']")
+
+        await user.setValue("Admin")
+
+        await (await $("input[placeholder ='Password']")).setValue("admin1234")
+
+        await (await $("//button[contains(@class,'login')]")).click()
+
+        const msg = await $("//p[contains(@class,'alert-content')]").getText()
+
+        expect(msg).toHaveTextContaining("Invalid credentials")
+
+    });
+});
